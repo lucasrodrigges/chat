80d35c6e2dee38375cd8da5d34e4f5a1ea8dd3bf
wan lucas <wan.lucas.90@gmail.com>
2022-11-07 20:06:23 -0300

    :card_file_box: criando estrutura inicial do banco de dados

---
+++ db/db.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE chat;
+
+USE chat;
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL,
+  name VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  picture VARCHAR(200),
+  PRIMARY KEY (id)
+) ENGINE=innodb;
+
+CREATE TABLE connections (
+  user1_id INT UNSIGNED NOT NULL,
+  user2_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY (user1_id, user2_id),
+  FOREIGN KEY (user1_id) REFERENCES users (id),
+  FOREIGN KEY (user2_id) REFERENCES users (id)
+) ENGINE=innodb;
+
+CREATE TABLE messages (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  content TEXT NOT NULL,
+  from_user INT UNSIGNED NOT NULL,
+  to_user INT UNSIGNED NOT NULL,
+  sent_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  read_date DATETIME,
+  deleted DATETIME,
+  PRIMARY KEY (id),
+  FOREIGN KEY (from_user) REFERENCES users (id),
+  FOREIGN KEY (to_user) REFERENCES users (id)
+) ENGINE=innodb;
