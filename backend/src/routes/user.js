73a844688c80dbf37c249ff164447408815a02c9
wan lucas <wan.lucas.90@gmail.com>
2022-11-08 22:06:18 -0300

    :recycle: refatorando model

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const userDB = require('../db/userDB');
+const userModel = require('../model/user.model');
 
 const { validateNewUser } = require('../utils/validations');
 
 const route = express.Router();
 
 route.get('/', async (req, res) => {
-  const [users] = await userDB.getUsers();
+  const [users] = await userModel.findAll();
 
   res.status(200).json(users);
 });
 
 route.post('/', validateNewUser, (req, res) => {
-  userDB.addUser(req.body);
+  userModel.addUser(req.body);
 
   res.status(200).end();
 });
