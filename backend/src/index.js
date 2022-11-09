bf28864a0796a3ac5ee8a85bc9e3faf648f43e47
Lucas Rodrigues <rodrigges@Lucass-iMac-Pro.local>
2022-11-08 22:38:28 -0300

    :whale: adding dockerfiles and creating docker-compose.yml

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const connection = require('./db/connection');
+require('dotenv').config();
 
 const userRoute = require('./routes/user');
 
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
 
 app.use(express.json());
 app.use('/user', userRoute);
 
 app.listen(port, async () => {
+  // eslint-disable-next-line no-console
   console.log('online');
 
   const [response] = await connection.execute('SELECT 1');
+  // eslint-disable-next-line no-console
   if (response) console.log('Conectado ao banco de dados');
 });
