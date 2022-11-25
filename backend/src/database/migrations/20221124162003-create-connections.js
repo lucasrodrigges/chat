4a8a02259b1870ba069c3bfaa5b53b5b2b9bda16
wan lucas <wan.lucas.90@gmail.com>
2022-11-25 10:59:48 -0300

    :sparkles: criando tabela connections

--- backend/src/database/migrations/20221124162003-create-connections.js
+++ backend/src/database/migrations/20221124162003-create-connections.js
@@ -3,19 +3,13 @@ module.exports = {
     await queryInterface.createTable('connections', {
       user1_id: {
         type: Sequelize.INTEGER,
-        primaryKey: true,
         allowNull: false,
         references: { model: 'users', key: 'id' },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
       },
       user2_id: {
         type: Sequelize.INTEGER,
-        primaryKey: true,
         allowNull: false,
         references: { model: 'users', key: 'id' },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
       },
       created_At: {
         type: Sequelize.DATE,
