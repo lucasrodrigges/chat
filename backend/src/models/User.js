41403f4645b2fd93deacd80c7cf5a38b70b8e81e
wan lucas <wan.lucas.90@gmail.com>
2022-11-26 13:32:17 -0300

    :recycle: refatorando service connection

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, QueryTypes } = require('sequelize');
 
 class User extends Model {
   static init(sequelize) {
@@ -11,11 +11,25 @@ class User extends Model {
 
   static associate(models) {
     this.belongsToMany(models.User, {
-      as: 'friend',
+      as: 'friends',
       through: 'connections',
       foreignKey: 'user1_id',
       otherKey: 'user2_id',
     });
+  }
+
+  async getReciprocalFriends() {
+    const friends = await this.sequelize.query(`
+      SELECT 
+        u.name, u.id, u.picture
+      FROM chat.connections AS c1
+        INNER JOIN chat.connections AS c2
+          ON c2.user2_id = c1.user1_id AND c1.user2_id = c2.user1_id
+        INNER JOIN chat.users AS u
+          ON c1.user2_id = u.id
+      WHERE c1.user1_id = ${this.id}`, { type: QueryTypes.SELECT });
+
+    return friends;
   }
 }
 
