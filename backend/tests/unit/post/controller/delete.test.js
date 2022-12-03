451aff208c14553ca26a8315a71bc6420065a945
wan lucas <wan.lucas.90@gmail.com>
2022-12-03 15:39:41 -0300

    :white_check_mark: cobertura de testes na camada controller do post

---
+++ backend/tests/unit/post/controller/delete.test.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+
+const postController = require('../../../../src/controllers/post.controller');
+const postService = require('../../../../src/services/post.service');
+
+const mockController = require('../../helpers/mockController');
+const mocks = require('../mocks');
+
+chai.use(sinonChai);
+
+describe('DELETE /post/controller', () => {
+  afterEach(sinon.restore);
+
+  it('Sucesso na exclusão de post', async () => {
+    sinon.stub(postService, 'deletePost').resolves(mocks.service.sucess);
+
+    const headers = { userId: 20 };
+    const params = { id: 4 };
+    const { req, res } = mockController({ headers, params });
+
+    await postController.deletePost(req, res);
+
+    expect(postService.deletePost).to.calledWith(headers.userId, params.id);
+    expect(res.status).to.calledWith(200);
+    expect(res.end).to.calledWith();
+  });
+});
---
+++ backend/tests/unit/post/controller/get.test.js
@@ -0,0 +1,56 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+
+const postController = require('../../../../src/controllers/post.controller');
+const postService = require('../../../../src/services/post.service');
+
+const mockController = require('../../helpers/mockController');
+const mocks = require('../mocks');
+
+chai.use(sinonChai);
+
+describe('GET /post/controller', () => {
+  afterEach(sinon.restore);
+
+  it('Sucesso na obtenção de post por id', async () => {
+    sinon.stub(postService, 'getPostById').resolves(mocks.service.sucess);
+
+    const params = { id: 4 };
+    const { req, res } = mockController({ params });
+
+    await postController.getPostById(req, res);
+
+    expect(postService.getPostById).to.calledWith(params.id);
+    expect(res.status).to.calledWith(200);
+    expect(res.json).to.calledWith(mocks.controller.sucess);
+  });
+
+  it('Sucesso na obtenção de posts por usuário', async () => {
+    sinon.stub(postService, 'getPostsByUser').resolves(mocks.service.sucess);
+
+    const params = { id: 4 };
+    const { req, res } = mockController({ params });
+
+    await postController.getPostsByUser(req, res);
+
+    expect(postService.getPostsByUser).to.calledWith(params.id);
+    expect(res.status).to.calledWith(200);
+    expect(res.json).to.calledWith(mocks.controller.sucess);
+  });
+
+  it('Sucesso na obtenção de posts por amigos', async () => {
+    sinon.stub(postService, 'getPostsByFriends').resolves(mocks.service.sucess);
+
+    const params = { id: 4 };
+    const { req, res } = mockController({ params });
+
+    await postController.getPostsByFriends(req, res);
+
+    expect(postService.getPostsByFriends).to.calledWith(params.id);
+    expect(res.status).to.calledWith(200);
+    expect(res.json).to.calledWith(mocks.controller.sucess);
+  });
+});
