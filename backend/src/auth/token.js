53d1d77eb1149cc75b6f97ea26a1786a42b3c59e
wan lucas <wan.lucas.90@gmail.com>
2022-12-03 08:02:42 -0300

    :lock: adição de funções de criação e validação de token

---
+++ backend/src/auth/token.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const key = process.env.JWT_KEY;
+const config = {
+  algorithm: 'HS256',
+  expiresIn: '3d',
+};
+
+module.exports = {
+  createToken: (payload) => jwt.sign(payload, key, config),
+
+  validateToken: (token) => {
+    try {
+      return jwt.verify(token, key);
+    } catch (error) {
+      return { error: 'INVALID_TOKEN' };
+    }
+  },
+};
