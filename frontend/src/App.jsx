0a95aef8194acb891679cb52ca68eeb3b3826182
Lucas Rodrigues <rodrigges@pop-os.localdomain>
2022-12-13 11:24:28 -0300

    :sparkles: criando esqueleto para feed e componente Menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Chat from './pages/Chat';
+import Feed from './pages/Feed';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 export default function App() {
   const router = createBrowserRouter([{
@@ -10,6 +13,15 @@ export default function App() {
   }, {
     path: '/home',
     element: <Home />,
+  }, {
+    path: '/feed',
+    element: <Feed />,
+  }, {
+    path: '/chat',
+    element: <Chat />,
+  }, {
+    path: '/profile',
+    element: <Profile />,
   }]);
 
   return (
