de3671d6cc0c0c9f65b752c9c6d213e944204b36
wan lucas <wan.lucas.90@gmail.com>
2022-12-13 13:26:29 -0300

    :fire: remoção de arquivos criados por engano

--- backend/src/models/routers/index.js
+++
@@ -1,9 +0,0 @@
-const userRouter = require('./user.router');
-const postRouter = require('./post.router');
-const messageRouter = require('./message.route');
-
-module.exports = {
-  userRouter,
-  postRouter,
-  messageRouter,
-};
