9532b0d09c364675a27b9bc7a76408d0873c3093
Lucas Rodrigues <rodrigges@pop-os.localdomain>
2022-12-15 16:36:10 -0300

    :sparkles: preenchendo feed

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,16 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import PostCard from '../components/PostCard';
 import { GlobalContext } from '../context/GlobalProvider';
 
 import './Feed.css';
 
 export default function Feed() {
-  const { posts: { feed } } = useContext(GlobalContext);
+  const { posts: { feed }, getFeed } = useContext(GlobalContext);
+
+  useEffect(() => getFeed(), []);
 
   return (
     <div className="feed">
       {feed.map((post) => (
-        <PostCard post={post} />
+        <PostCard key={post.id} post={post} />
       ))}
     </div>
   );
