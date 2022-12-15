e69c38d7aa1e76dee1fec2e80a9049301f5c30b9
wan lucas <wan.lucas.90@gmail.com>
2022-12-15 19:55:28 -0300

    :art: atualização de estrutura para estilização

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,22 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PostCard from '../components/PostCard';
 import { GlobalContext } from '../context/GlobalProvider';
 
+import './Home.css';
+
 export default function Home() {
   const { posts: { trends }, getTrends } = useContext(GlobalContext);
+  const [posts, setPosts] = useState();
 
   useEffect(() => getTrends(), []);
 
   return (
     <div className="home">
-      {trends.length && trends.map((post) => (
-        <PostCard post={post} key={post.id} />
-      ))}
+      <ul className="posts">
+        {trends.length && trends.map((post) => (
+          <PostCard post={post} key={post.id} />
+        ))}
+      </ul>
     </div>
   );
 }
