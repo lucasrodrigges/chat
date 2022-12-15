0110cdddfcc277d5bca1c471aa471e3b8aa45505
Lucas Rodrigues <rodrigges@pop-os.localdomain>
2022-12-15 18:45:47 -0300

    :sparkles: adicionando tela de profile de usuários da plataforma

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -4,6 +4,7 @@ import Chat from './pages/Chat';
 import Feed from './pages/Feed';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import OtherProfile from './pages/OtherProfile';
 import Profile from './pages/Profile';
 
 export default function Router() {
@@ -14,6 +15,7 @@ export default function Router() {
       <Route path="/feed" element={<Feed />} />
       <Route path="/chat" element={<Chat />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/profile/:id" element={<OtherProfile />} />
     </Routes>
   );
 }
