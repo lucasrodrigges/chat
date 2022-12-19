db56213018ee5f122d13cf4c28dcf7b6c9af09cf
wan lucas <wan.lucas.90@gmail.com>
2022-12-19 10:14:28 -0300

    :lipstick: alterações no estilo

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -72,7 +72,9 @@ export default function Sidebar() {
             <p>{`@${user.userName}`}</p>
           </div>
         </div>
-        <span>...</span>
+        <svg viewBox="0 0 128 512">
+          <path d="M64 360c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zm0-160c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zM120 96c0 30.9-25.1 56-56 56S8 126.9 8 96S33.1 40 64 40s56 25.1 56 56z" fill="currentColor" />
+        </svg>
       </div>
     </section>
   );
