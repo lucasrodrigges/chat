9f3679e31260e752d2087759a2a5b12969b36cf0
lucasrodrigges <rodriggeslucas@gmail.com>
2023-01-05 16:09:38 -0300

    :sparkles: adicionando lógica de dar likes nos posts do feed e home

--- frontend/src/components/cards/ProfileCard.jsx
+++ frontend/src/components/cards/ProfileCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FriendRequestBtn from '../buttons/FriendRequestBtn';
 
 export default function ProfileCard({ user, isAdmin }) {
   return (
@@ -11,9 +12,9 @@ export default function ProfileCard({ user, isAdmin }) {
           <span>{`${user.followers} followers`}</span>
           <span>{`${user.following} following`}</span>
         </div>
-        {isAdmin ? <button type="button">Edit profile</button> : <button type="button">Follow</button>}
+        {isAdmin ? <button type="button">Edit profile</button> : <FriendRequestBtn />}
       </div>
-      <img className="user_image-circle" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="woman" />
+      <img className="small_circle" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="woman" />
     </div>
   );
 }
